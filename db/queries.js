e4b48a0d91e97a2c2003741b94df7acf5b5f1cb4
unknown <atiqit52@gmail.com>
2024-11-17 22:16:39 +0600

    events data added in event card

---
+++ db/queries.js
@@ -0,0 +1,12 @@
+// const { eventModel } = require("@/models/event-models");
+// 
+import { eventModel } from  "@/models/event-models";
+import { replaceMongoIdInArray } from "@/utils/data-util";
+
+
+async function getAllEvents (){
+    const allEvents = await eventModel.find().lean();
+    return replaceMongoIdInArray(allEvents);
+}
+
+export {getAllEvents}
